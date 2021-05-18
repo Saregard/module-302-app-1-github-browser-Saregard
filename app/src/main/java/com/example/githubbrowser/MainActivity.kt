9696b719291b49282e6ab2daa47ef9f0ffb867d1
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-18 12:13:38 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/githubbrowser/MainActivity.kt
+++ app/src/main/java/com/example/githubbrowser/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.githubbrowser
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
 
     private var editTextUsername: EditText? = null
     private var progressBar: ProgressBar? = null
+    private var imm: InputMethodManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,8 @@ class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
 
         editTextUsername = findViewById(R.id.editTextUsername)
         progressBar = findViewById(R.id.progressBar)
+
+        imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         editTextUsername?.setOnEditorActionListener(this)
 
@@ -33,6 +38,7 @@ class MainActivity : AppCompatActivity(), TextView.OnEditorActionListener {
             if(username.isEmpty() || username.isBlank()) {
                 editTextUsername?.error = getString(R.string.username_cannot_be_empty)
             }else {
+                imm?.hideSoftInputFromWindow(editTextUsername?.windowToken, 0)
                 progressBar?.visibility = View.VISIBLE
                 getRepositoriesForUsername(username)
             }
