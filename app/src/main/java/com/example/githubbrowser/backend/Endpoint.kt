cd6df2a73a7f937cc0c93b870716ea9d3f8755bc
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-18 12:43:10 +0200

    Update Endpoint.kt

--- app/src/main/java/com/example/githubbrowser/backend/Endpoint.kt
+++ app/src/main/java/com/example/githubbrowser/backend/Endpoint.kt
@@ -1,4 +1,9 @@
 package com.example.githubbrowser.backend
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface Endpoint {
+    @GET("users/{user}/repos")
+    fun getRepositoriesForUser(@Path("user") username: String)
 }
