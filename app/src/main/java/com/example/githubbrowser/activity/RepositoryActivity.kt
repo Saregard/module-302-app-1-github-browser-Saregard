a7e78e03d7758703b8b8c5b003848fd6d61de1b6
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-19 11:39:06 +0200

    Update RepositoryActivity.kt

--- app/src/main/java/com/example/githubbrowser/activity/RepositoryActivity.kt
+++ app/src/main/java/com/example/githubbrowser/activity/RepositoryActivity.kt
@@ -3,6 +3,7 @@ package com.example.githubbrowser.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.githubbrowser.R
 import com.example.githubbrowser.model.Repository
 
@@ -13,7 +14,10 @@ class RepositoryActivity : AppCompatActivity() {
 
         val listOfRepos = intent?.getParcelableArrayListExtra<Repository>(KEY_REPOSITORY_DATA)
         listOfRepos?.let {
+            val numberOfRepositories = it.size
 
+            val textViewNumbersOfRepos = findViewById<TextView>(R.id.textViewNumberOfRepos)
+            textViewNumbersOfRepos?.text =
         }
     }
     companion object{
