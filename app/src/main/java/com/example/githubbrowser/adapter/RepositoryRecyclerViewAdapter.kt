af18a5e42da2272a69869dfffc53cc3d67ad5d38
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-19 11:17:48 +0200

    Create RepositoryRecyclerViewAdapter.kt

---
+++ app/src/main/java/com/example/githubbrowser/adapter/RepositoryRecyclerViewAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.githubbrowser.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RepositoryRecyclerViewAdapter {
+
+
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        
+    }
+}
