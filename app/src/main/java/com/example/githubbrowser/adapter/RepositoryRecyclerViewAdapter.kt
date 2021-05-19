796bf4e580a32dbec47ad8ba1102f0fdcf73d0c3
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-19 11:24:56 +0200

    Update RepositoryRecyclerViewAdapter.kt

--- app/src/main/java/com/example/githubbrowser/adapter/RepositoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/githubbrowser/adapter/RepositoryRecyclerViewAdapter.kt
@@ -1,13 +1,29 @@
 package com.example.githubbrowser.adapter
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.githubbrowser.R
+import com.example.githubbrowser.model.Repository
+import kotlinx.android.synthetic.main.layout_recycler_view_repository.view.*
 
 class RepositoryRecyclerViewAdapter {
 
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-        
+        private var textViewRepoName = view.findViewById<TextView>(R.id.textViewRepoName)
+        private var textViewPrivacy = view.findViewById<TextView>(R.id.textViewPrivacy)
+        private var textViewLicence = view.findViewById<TextView>(R.id.textViewLicence)
+
+        fun bind (repository: Repository) {
+            textViewRepoName?.text = repository.name
+            textViewLicence?.text = repository.license
+            
+        }
+
+
+
+
     }
 }
